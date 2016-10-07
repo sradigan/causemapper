da7aaace8bed216635e495a18ba6732ac948eae7
Sean Radigan <srradigan@gmail.com>
2016-10-07 09:08:46 -0400

    Major refactoring, using objects now

--- genmap.py
+++ genmap.py
@@ -4,6 +4,13 @@ import sys
 import json
 from textwrap import fill
 from optparse import OptionParser
+class map_node:
+    def __init__(self):
+        self.name = ''
+        self.content = ''
+        self.solutions = list()
+        self.evidence = list()
+        self.deps = set()
 
 class dot_map:
     def __init__(self):
@@ -11,16 +18,19 @@ class dot_map:
                     'graph [rankdir=RL, splines=polyline,'\
                            'arrowType=normal, style=normal]\n'\
                     'node [shape=record, margin=0];\n'
+        self.nodes = list()
 
-    def add_node_text(self, name, content,
-                      solutions=None, evidence=None, linew=15):
+    def node_by_name(self, name):
+        for n in self.nodes:
+            if n.name == name:
+                return n
+        return None
+
+    def add_node_text(self, node, linew=15):
         """Add a node's text to the graphviz output.
 
         Args:
-            name (str): The name of the graphviz map node.
-            content (str): The main content text of the node.
-            solutions (list of str): List of solution strings.
-            evidence (list of str): List of evidence strings.
+            node (map_node): The node to format into graphviz text
             linew (num): Max characters per line, longer will be wrapped.
 
         """
@@ -28,12 +38,12 @@ class dot_map:
 
         # Setup header text
         nText = '{0} [label=<\n'\
-                '\t<table {1}>\n'.format(name, tprops)
+                '\t<table {1}>\n'.format(node.name, tprops)
 
-        if solutions is not None:
+        if 0 < len(node.solutions):
             # Assemble solutions
             srows = ""
-            for s in solutions:
+            for s in node.solutions:
                 ws = fill(s, width=linew).replace("\n", "<br/>")
                 srows += '\t\t<tr><td bgcolor="aquamarine">{0}</td></tr>\n'.format(ws)
 
@@ -41,13 +51,13 @@ class dot_map:
             nText += srows
 
         # Add in main content
-        wcontent = fill(content, width=linew).replace("\n", "<br/>")
+        wcontent = fill(node.content, width=linew).replace("\n", "<br/>")
         nText += '\t\t<tr><td port="f0">{0}</td></tr>\n'.format(wcontent)
 
-        if evidence is not None:
+        if 0 < len(node.evidence):
             # Assemble evidence
             erows = ""
-            for e in evidence:
+            for e in node.evidence:
                 we = fill(e, width=linew).replace("\n", "<br/>")
                 erows += '\t\t<tr><td bgcolor="lightpink">{0}</td></tr>\n'.format(we)
 
@@ -59,58 +69,60 @@ class dot_map:
                  '> shape=none ];\n'
         self.text += nText
 
-    def add_node_linkage_text(self, dep_tree):
+    def add_node_linkage_text(self):
         """Add node linkage text to the graphviz output
-
-        Args:
-            dep_tree (dict): A dictionary containing a node dependency tree
 
         """
         # Generate node linkage and dummy node linkage
         # dummy nodes "joints" are used to unify lines going to the same node
         dep_text = ''
-        for n in dep_tree:
+        for n in self.nodes:
+            if 0 == len(n.deps):
+                continue
+
             dep_text += (
                 '{0}_joint[shape="none", label="", width=0, height=0];\n'
-                '{0}_joint -> {0}:f0:e;\n').format(n)
-            for d in dep_tree[n]:
-                dep_text += '{0}:f0:w -> {1}_joint[arrowhead="none"];\n'.format(d, n)
+                '{0}_joint -> {0}:f0:e;\n').format(n.name)
+            for d in n.deps:
+                dep_text += '{0}:f0:w -> {1}_joint[arrowhead="none"];\n'.format(d, n.name)
 
         self.text += dep_text
 
-    def add_rank_text(self, dep_tree):
+    def add_rank_text(self):
         """Add node ranking text to the graphviz output
-
-        Args:
-            dep_tree (dict): A dictionary containing a node dependency tree
 
         """
         rank_dict = {}
-        # get list of all nodes
-        key_set = set(dep_tree.keys())
-        value_set = set()
-        # combine all node dependencies into one set
-        for i in dep_tree.values():
-            value_set |= set(i)
+        key_set = set()
+        dep_set= set()
+        for n in self.nodes:
+            # get list of all nodes
+            key_set |= {n.name}
+            # combine all node dependencies into one set
+            dep_set |= n.deps
+
         # get only nodes that are not dependencies of others
-        rank_dict[0] = key_set - value_set
+        rank_dict[0] = key_set - dep_set
         # Rank all other nodes
         irank = 0
         done = False
         while not done:
             done = True
-            irank_dict = rank_dict[irank]
+            irank_set = rank_dict[irank]
             irankn = irank + 1
-            for irn in irank_dict:
-                if not irn in dep_tree:
+            for irn in irank_set:
+                node = self.node_by_name(irn)
+                if node is None:
                     continue
-                for n in dep_tree[irn]:
-                    if not irankn in rank_dict:
-                        rank_dict[irankn] = set()
-                    # add the node to this rank
-                    rank_dict[irankn] |= {n}
-                    done = False
+                if 0 == len(node.deps):
+                    continue
+                if not irankn in rank_dict:
+                    rank_dict[irankn] = set()
+                rank_dict[irankn] |= node.deps
+                done = False
+
             irank = irankn
+
         # Ensure nodes are only specified in 1 rank
         for i in range(irank-1,-1,-1):
             for j in range(0,i):
@@ -133,10 +145,6 @@ class dot_map:
         """
         dep_tree = {}
         for n in m['nodes']:
-            # Add text for each node
-            e = None
-            s = None
-
             # Ensure minimal field requirements
             if not 'name' in n:
                 print('Error: "name" missing from node.\n')
@@ -148,32 +156,38 @@ class dot_map:
                 )
                 sys.exit(-1)
 
+            node = map_node()
+            node.name = n['name']
+            node.content = n['content']
+
             if 'evidence' in n:
-                e = n['evidence']
+                node.evidence = n['evidence']
 
             if 'solutions' in n:
-                s = n['solutions']
+                node.solutions = n['solutions']
 
-            self.add_node_text(n['name'], n['content'], s, e)
-
-            # Create a dependency tree
+            # Load in dependencies we know about
             if 'effectedby' in n:
-                if not n['name'] in dep_tree:
-                    dep_tree[n['name']] = n['effectedby']
-                else:
-                    no_dups = set(dep_tree[n['name']] + n['effectedby'])
-                    dep_tree[n['name']] = list(no_dups)
+                node.deps |= set(n['effectedby'])
 
+            self.nodes.append(node)
+
+        # Load the rest of dependencies
+        for n in m['nodes']:
             if 'effects' in n:
                 for en in n['effects']:
-                    if not en in dep_tree:
-                        dep_tree[en] = []
+                    node = self.node_by_name(en)
+                    if node is None:
+                        print('Error: In {0}, {1} listed under effects, but does not exist'.format(n['name'], en))
+                        sys.exit(-1)
 
-                    if not n['name'] in dep_tree[en]:
-                        dep_tree[en].append(n['name'])
+                    node.deps.add(n['name'])
 
-        self.add_node_linkage_text(dep_tree)
-        self.add_rank_text(dep_tree)
+        for node in self.nodes:
+            self.add_node_text(node)
+
+        self.add_node_linkage_text()
+        self.add_rank_text()
 
         self.text += '}\n'
 
